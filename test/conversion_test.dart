b83fd75f44a7b3ec8c3a301b0c51296efaced694
Damiano <damianoferrari1998@gmail.com>
2021-03-01 19:29:05 +0100

    Added new conversions

--- test/conversion_test.dart
+++ test/conversion_test.dart
@@ -3,11 +3,31 @@ import 'package:units_converter/units_converter.dart';
 
 /// This function defines if a value is accettable. e.g. if we expect to have 1 but we get 1.00000000012, is this a valid result or not?
 /// The term sensbility is used improperly.
-bool isAcceptable(double convertedValue, double expectedValue, {double sensibility = 1e10}) {
+bool isAcceptable(double convertedValue, double expectedValue, sensibility) {
   final double accuracy = expectedValue / sensibility;
   final double upperConstraint = expectedValue + accuracy;
   final double lowerConstraint = expectedValue - accuracy;
   return convertedValue >= lowerConstraint && convertedValue <= upperConstraint;
+}
+
+void runConversionTest(Map<dynamic, double> expectedResult, Property property, {double sensibility = 1e10}) {
+  final List listNames = expectedResult.keys.toList();
+  for (var unitName in listNames) {
+    test('Test from ${unitName.toString()}', () {
+      property.convert(unitName, expectedResult[unitName]);
+      List<Unit> unitList = property.getAll();
+      for (Unit unit in unitList) {
+        var name = unit.name;
+        double convertedValue = unitList.where((element) => element.name == name).single.value!;
+        expect(
+          isAcceptable(convertedValue, expectedResult[name]!, sensibility),
+          true,
+          reason: 'Error with ${name.toString()}. Expected: ${expectedResult[name]}, result: ${convertedValue}',
+        );
+      }
+    });
+    property.convert(listNames[0], null); //clear all values
+  }
 }
 
 void main() {
@@ -18,20 +38,7 @@ void main() {
       ANGLE.seconds: 3600,
       ANGLE.radians: 0.01745329252,
     };
-    final List<ANGLE> listNames = expectedResult.keys.toList();
-    Angle property = Angle();
-    for (ANGLE unitName in listNames) {
-      test('Test from ${unitName.toString()}', () {
-        property.convert(unitName, expectedResult[unitName]);
-        List<Unit> unitList = property.getAll();
-        for (Unit unit in unitList) {
-          ANGLE name = unit.name;
-          double convertedValue = unitList.where((element) => element.name == name).single.value!;
-          expect(isAcceptable(convertedValue, expectedResult[name]!), true, reason: 'Error with ${name.toString()}');
-        }
-      });
-      property.convert(listNames[0], null); //clear all values
-    }
+    runConversionTest(expectedResult, Angle());
   });
 
   group('Area conversion', () {
@@ -48,20 +55,7 @@ void main() {
       AREA.acres: 0.00024710538146717,
       AREA.are: 0.01,
     };
-    final List<AREA> listNames = expectedResult.keys.toList();
-    Area property = Area();
-    for (AREA unitName in listNames) {
-      test('Test from ${unitName.toString()}', () {
-        property.convert(unitName, expectedResult[unitName]);
-        List<Unit> unitList = property.getAll();
-        for (Unit unit in unitList) {
-          AREA name = unit.name;
-          double convertedValue = unitList.where((element) => element.name == name).single.value!;
-          expect(isAcceptable(convertedValue, expectedResult[name]!), true, reason: 'Error with ${name.toString()}');
-        }
-      });
-      property.convert(listNames[0], null); //clear all values
-    }
+    runConversionTest(expectedResult, Area());
   });
 
   group('Digital data conversion', () {
@@ -94,19 +88,108 @@ void main() {
       DIGITAL_DATA.pebibyte: 8.8817841970013e-16,
       DIGITAL_DATA.exbibyte: 8.673617379884e-19,
     };
-    final List<DIGITAL_DATA> listNames = expectedResult.keys.toList();
-    DigitalData property = DigitalData();
-    for (DIGITAL_DATA unitName in listNames) {
-      test('Test from ${unitName.toString()}', () {
-        property.convert(unitName, expectedResult[unitName]);
-        List<Unit> unitList = property.getAll();
-        for (Unit unit in unitList) {
-          DIGITAL_DATA name = unit.name;
-          double convertedValue = unitList.where((element) => element.name == name).single.value!;
-          expect(isAcceptable(convertedValue, expectedResult[name]!), true, reason: 'Error with ${name.toString()}');
-        }
-      });
-      property.convert(listNames[0], null); //clear all values
-    }
+    runConversionTest(expectedResult, DigitalData());
+  });
+
+  group('Energy', () {
+    const Map<ENERGY, double> expectedResult = {
+      ENERGY.joules: 1,
+      ENERGY.calories: 0.2388459,
+      ENERGY.kilowatt_hours: 2.7777777778e-7,
+      ENERGY.electronvolts: 6.2415097523028e18,
+    };
+    runConversionTest(expectedResult, Energy());
+  });
+
+  group('Force', () {
+    const Map<FORCE, double> expectedResult = {
+      FORCE.newton: 1,
+      FORCE.dyne: 1e5,
+      FORCE.pound_force: 0.22480894309971,
+      FORCE.kilogram_force: 0.10197162129779,
+      FORCE.poundal: 7.2330138512099,
+    };
+    runConversionTest(expectedResult, Force());
+  });
+
+  group('Fuel consumption', () {
+    const Map<FUEL_CONSUMPTION, double> expectedResult = {
+      FUEL_CONSUMPTION.kilometers_per_liter: 1,
+      FUEL_CONSUMPTION.liters_per_100_km: 100,
+      FUEL_CONSUMPTION.miles_per_US_gallon: 2.3521458335008,
+      FUEL_CONSUMPTION.miles_per_imperial_gallon: 2.8248093635947,
+    };
+    runConversionTest(expectedResult, FuelConsumption());
+  });
+
+  group('Lenght', () {
+    const Map<LENGTH, double> expectedResult = {
+      LENGTH.meters: 1,
+      LENGTH.centimeters: 100,
+      LENGTH.inches: 39.370078740157,
+      LENGTH.feet: 3.2808398950131,
+      LENGTH.nautical_miles: 0.00053995680345572,
+      LENGTH.yards: 1.0936132983377,
+      LENGTH.miles: 0.00062137119223733,
+      LENGTH.millimeters: 1e3,
+      LENGTH.micrometers: 1e6,
+      LENGTH.nanometers: 1e9,
+      LENGTH.angstroms: 1e10,
+      LENGTH.picometers: 1e12,
+      LENGTH.kilometers: 1e-3,
+      LENGTH.astronomical_units: 6.6845871222684e-12,
+      LENGTH.light_years: 1.057000451015e-16,
+      LENGTH.parsec: 3.2423326718251e-17,
+    };
+    runConversionTest(expectedResult, Length());
+  });
+
+  group('Mass', () {
+    const Map<MASS, double> expectedResult = {
+      MASS.grams: 1,
+      MASS.ettograms: 1e-2,
+      MASS.kilograms: 1e-3,
+      MASS.pounds: 0.0022046226218488,
+      MASS.ounces: 0.03527396194958,
+      MASS.quintals: 0.00001,
+      MASS.tons: 0.000001,
+      MASS.milligrams: 1e3,
+      MASS.uma: 6.0221410036139e23,
+      MASS.carats: 5,
+      MASS.centigrams: 1e2,
+      MASS.pennyweights: 0.6430149314,
+      MASS.troy_ounces: 0.03215074657,
+      MASS.stones: 0.0001574730444,
+    };
+    runConversionTest(expectedResult, Mass(), sensibility: 1e9);
+  });
+
+  group('Power', () {
+    const Map<POWER, double> expectedResult = {
+      POWER.watt: 1,
+      POWER.milliwatt: 1000,
+      POWER.kilowatt: 1e-3,
+      POWER.megawatt: 1e-6,
+      POWER.gigawatt: 1e-9,
+      POWER.european_horse_power: 0.0013596216173039,
+      POWER.imperial_horse_power: 0.0013410220895991,
+    };
+    runConversionTest(expectedResult, Power());
+  });
+
+  group('Shoe size', () {
+    const Map<SHOE_SIZE, double> expectedResult = {
+      SHOE_SIZE.centimeters: 25.167,
+      SHOE_SIZE.inches: 9.908,
+      SHOE_SIZE.eu_china: 40,
+      SHOE_SIZE.uk_india_child: 19.724,
+      SHOE_SIZE.uk_india_man: 6.724,
+      SHOE_SIZE.uk_india_woman: 6.224,
+      SHOE_SIZE.usa_canada_child: 13.391,
+      SHOE_SIZE.usa_canada_man: 7.724,
+      SHOE_SIZE.usa_canada_woman: 8.724,
+      SHOE_SIZE.japan: 26.667,
+    };
+    runConversionTest(expectedResult, ShoeSize(), sensibility: 1e1);
   });
 }
