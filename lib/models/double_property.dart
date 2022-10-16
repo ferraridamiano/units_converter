00406c2cadad030d5c0a250526fbc756e8164aa1
Damiano Ferrari <damianoferrari1998@gmail.com>
2022-10-16 16:28:44 +0100

    Revert "Initial setup for compound units"
    This reverts commit b8ee417d65b6fe2958c04905865c1f6005d3d648.

--- lib/models/double_property.dart
+++ lib/models/double_property.dart
@@ -4,32 +4,6 @@ import 'package:units_converter/models/unit.dart';
 import 'package:units_converter/utils/utils.dart';
 
 abstract class DoubleProperty<T> extends Property<T, double> {
-  /// Defines the relation between the units of measurement of this property.
-  /// E.g. in the following example we defined: `KiloDash = 1000 * Dash`,
-  /// `DashPlus1 = Dash + 1` and `OneOver(DashPlus1) = 1 / (DashPlus1)`:
-  /// ```dart
-  /// ConversionNode conversionTree = ConversionNode(
-  ///   name: 'Dash',
-  ///   leafNodes: [
-  ///     ConversionNode(
-  ///       name: 'KiloDash',
-  ///       coefficientProduct: 1000,
-  ///     ),
-  ///     ConversionNode(
-  ///       name: 'DashPlus1',
-  ///       coefficientSum: -1,
-  ///       leafNodes: [
-  ///         ConversionNode(
-  ///           name: 'OneOver(DashPlus1)',
-  ///           conversionType: ConversionType.reciprocalConversion,
-  ///         ),
-  ///       ],
-  ///     ),
-  ///   ],
-  /// );
-  /// ```
-  ConversionNode<T> conversionTree;
-
   /// Map between units and its symbol
   Map<T, String>? mapSymbols;
 
@@ -49,13 +23,13 @@ abstract class DoubleProperty<T> extends Property<T, double> {
   late List<ConversionNode> _nodeList;
 
   DoubleProperty(
-      {required this.conversionTree,
+      {required ConversionNode<T> conversionTree,
       this.mapSymbols,
       name,
       this.significantFigures = 10,
       this.removeTrailingZeros = true,
       this.useScientificNotation = true}) {
-    conversionTree = conversionTree;
+    super.conversionTree = conversionTree;
     this.name = name;
     _nodeList = conversionTree.getTreeAsList();
     size = _nodeList.length;
