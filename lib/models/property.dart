b8ee417d65b6fe2958c04905865c1f6005d3d648
Damiano Ferrari <damianoferrari1998@gmail.com>
2022-10-16 16:26:21 +0100

    Initial setup for compound units

--- lib/models/property.dart
+++ lib/models/property.dart
@@ -1,10 +1,9 @@
-import 'package:units_converter/models/conversion_node.dart';
-
 import 'unit.dart';
 
 enum PROPERTY {
   angle,
   area,
+  density,
   digitalData,
   energy,
   force,
@@ -31,32 +30,6 @@ abstract class Property<K, V> {
   /// The size of the [Property], i.e. the number of the [Unit]s that belong to
   /// that [Property].
   late int size;
-
-  /// Defines the relation between the units of measurement of this property.
-  /// E.g. in the following example we defined: `KiloDash = 1000 * Dash`,
-  /// `DashPlus1 = Dash + 1` and `OneOver(DashPlus1) = 1 / (DashPlus1)`:
-  /// ```dart
-  /// ConversionNode conversionTree = ConversionNode(
-  ///   name: 'Dash',
-  ///   leafNodes: [
-  ///     ConversionNode(
-  ///       name: 'KiloDash',
-  ///       coefficientProduct: 1000,
-  ///     ),
-  ///     ConversionNode(
-  ///       name: 'DashPlus1',
-  ///       coefficientSum: -1,
-  ///       leafNodes: [
-  ///         ConversionNode(
-  ///           name: 'OneOver(DashPlus1)',
-  ///           conversionType: ConversionType.reciprocalConversion,
-  ///         ),
-  ///       ],
-  ///     ),
-  ///   ],
-  /// );
-  /// ```
-  late ConversionNode<K> conversionTree;
 
   /// Convert the [value] of the units with name [name] to all the other units.
   void convert(K name, V? value);
